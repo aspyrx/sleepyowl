56fceb1c4087e6da18d293b3194a84bc4bd21246
Stan Zhang <stan.zhang2@gmail.com>
2016-02-06 18:16:56 +0000

    switch to webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
         connect: {
             build: {
                 options: {
@@ -8,79 +7,32 @@ module.exports = function(grunt) {
                     base: 'build/',
                     livereload: true,
                     open: true
-                },
+                }
             }
         },
-        clean: {
-            build: ['build'],
-            dist: ['dist']
-        },
+
         watch: {
             options: {
-                livereload: true,
+                livereload: true
             },
-            react: {
+            webpack: {
                 files: ['src/**/*.js', 'src/**/*.jsx'],
-                tasks: ['browserify:build']
+                tasks: ['webpack:build']
             },
             less: {
-                files: 'src/less/*.less',
+                files: 'src/less/**/*.less',
                 tasks: ['less:build']
             },
-            html: {
-                files: 'src/**/*.html',
-                tasks: ['htmlmin:build']
+            copy: {
+                files: ['img/**', 'src/**/*.html'],
+                tasks: ['copy:build']
             }
         },
-        browserify: {
-            options: {
-                transform: [['babelify', {presets: ['react']}]]
-            },
-            build: {
-                src: ['src/**/*.js', 'src/**/*.jsx'],
-                dest: 'build/app.js'
-            }
+
+        clean: {
+            build: ['build/']
         },
-        less: {
-            build: {
-                options: {
-                    paths: 'src/'
-                },
-                files: {
-                    'build/style.css': 'src/less/main.less'
-                }
-            },
-        },
-        htmlmin: {
-            build: {
-                files: {
-                    'build/index.html': 'src/index.html'
-                }
-            },
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: {
-                    'dist/index.html': 'src/index.html'
-                }
-            }
-        },
-        uglify: {
-            dist: {
-                files: {
-                    'dist/app.js': 'build/app.js'
-                }
-            }
-        },
-        cssmin: {
-            dist: {
-                files: {
-                    'dist/style.css': 'build/style.css'
-                }
-            }
-        },
+
         copy: {
             build: {
                 files: [{
@@ -89,37 +41,70 @@ module.exports = function(grunt) {
                     dest: 'build/'
                 }, {
                     expand: true,
-                    cwd: 'node_modules/bootstrap/',
+                    cwd: 'src/',
+                    src: '**/*.html',
+                    dest: 'build/'
+                }, {
+                    expand: true,
+                    cwd: 'node_modules/bootstrap',
                     src: 'fonts/**',
                     dest: 'build/'
                 }]
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    src: 'img/**',
-                    dest: 'dist/'
-                }, {
-                    expand: true,
-                    cwd: 'node_modules/bootstrap/',
-                    src: 'fonts/**',
-                    dest: 'dist/'
-                }]
             }
         },
+
+        less: {
+            build: {
+                options: {
+                    paths: 'src/'
+                },
+                files: {
+                    'build/style.css': 'src/less/main.less'
+                }
+            }
+        },
+
+        webpack: {
+            build: {
+                entry: './src/index.js',
+                output: {
+                    path: 'build/',
+                    filename: 'app.js'
+                },
+                stats: {
+                    colors: true,
+                    modules: false,
+                    reasons: true
+                },
+                progress: true,
+                keepalive: true,
+                module: {
+                    loaders: [
+                        {
+                            test: /\.js?$/,
+                            exclude: /node_modules/,
+                            loader: 'babel',
+                            query: {
+                                cacheDirectory: true,
+                                presets: ['react', 'es2015']
+                            }
+                        }
+                    ]
+                }
+            },
+            devServer: {
+                contentBase: 'build/'
+            }
+        }
     });
 
-    grunt.loadNpmTasks('grunt-browserify');
-    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-webpack');
 
-    grunt.registerTask('build', ['clean:build', 'copy:build', 'browserify:build', 'less:build', 'htmlmin:build']);
-    grunt.registerTask('dist', ['clean:dist', 'copy:dist', 'build', 'htmlmin:dist', 'uglify:dist', 'cssmin:dist']);
+    grunt.registerTask('build', ['clean:build', 'copy:build', 'less:build', 'webpack:build' ]);
     grunt.registerTask('default', ['connect:build', 'watch']);
-};
+}
