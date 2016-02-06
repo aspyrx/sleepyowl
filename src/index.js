8da271b275787092256e8b93fc5ff78e844da760
Stan Zhang <stan.zhang2@gmail.com>
2016-02-06 04:35:16 +0000

    Updated skeleton code

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <h1>Hello world!</h1>
+    // your element here, e.g.
+    // <App prop1='foo'>Hello world</App>
+, document.getElementById('root'));
