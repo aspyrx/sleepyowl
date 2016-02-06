07c4c049c89deb29e6935ee5f013d98c8d2130cf
Stan Zhang <stan.zhang2@gmail.com>
2016-02-06 03:16:33 +0000

    Hello Bootstrap

---
+++ src/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Button = require('react-bootstrap').Button;
+
+ReactDOM.render(
+        <Button bsStyle="primary">Primary</Button>
+, document.getElementById('root'));
