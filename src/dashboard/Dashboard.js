0f7409f900ad7e9561b25176430d4d047b172d8b
Claire Yuan <claireyuan97@gmail.com>
2016-02-06 16:25:33 -0500

    spiffy title

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,3 +1,4 @@
+var Radium = require('radium');
 var React = require('react');
 var Row = require('react-bootstrap/lib/Row');
 var Col = require('react-bootstrap/lib/Col');
@@ -5,11 +6,21 @@ var Owl = require('./Owl');
 var HealthBar = require('./HealthBar');
 var Graph = require('./Graph');
 
+var styles = {
+    head: {
+        textAlign: 'center'
+    }
+};
+
 module.exports = React.createClass({
   render: function() {
     return (
             <Row>
                 <Col xs={12}>
+                    <Row>
+                        <h1 style={styles.head}>sleepy owl</h1>
+                        <hr />
+                    </Row>
                     <Row>
                         <Owl name={this.props.username} message={this.props.message} state={this.props.state} />
                     </Row>
