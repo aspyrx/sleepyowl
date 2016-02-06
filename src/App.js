59325df29023da4368bde20a689c1e4f2d9ff200
Neil Xu <neil.z.xu@gmail.com>
2016-02-06 15:02:51 -0500

    one ten is better than two

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ module.exports = React.createClass({
             {feedtype: 'card', name: 'Fire underling', start: '9:05', end: '9:25'},
             {feedtype: 'card', name: 'Strengthen the economy', start: '9:30', end: '10:00'},
             {feedtype: 'timeline', name: '7:00', start: '10:00', end: ''},
-            {feedtype: 'timeline', name: 'Fun Meeting', start: '10:00', end: '9:00'},
             {feedtype: 'alarm', name: 'Boring Meeting', start: '10:15', end: '9:00'},
             {feedtype: 'card', name: 'Boring Meeting', start: '10:05', end: '10:25'}
         ];
