bd8eaa0281a5a601acbc36434060986febab8b33
Stan Zhang <stan.zhang2@gmail.com>
2016-02-06 03:59:23 +0000

    Basic file structure

--- src/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-var HelloAlert = require('./HelloAlert');
-
-ReactDOM.render(
-        <HelloAlert message="World" />
-, document.getElementById('root'));
